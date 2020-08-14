f7d2ace804fa252ee7f127d319c7e3f58c0cdf06
Zmingfeng <xycg.sky@gmail.com>
2020-08-14 13:50:52 +0800

    RomanToInteger_Leetcode

--- CPlusPlus/RomanToInteger.cpp
+++ CPlusPlus/RomanToInteger.cpp
@@ -4,35 +4,24 @@
 #include <vector>
 using namespace std;
 
-
-int main(int args, string *argv[]){
-
-    string roman;
-    cout << 'Please input roman number:';
-    cin >> roman;
-    cout << endl;
-    cout << romanToInt(roman) << endl;
-    return 0;
-}
 int romanToInt(string s) {
     int output_number = 0;
     map<char, int> RomanToInteger{
         {'I', 1}, {'V', 5}, {'X', 10}, {'L', 50}, {'C', 100}, {'D', 500}, {'M', 1000}
     };
-    for (int i = 0; i++; i < s.length()){
-        switch (s[i] + s[i+1]){
-            case 'IV': 
-            case 'IX':
-            case 'XL':
-            case 'XC':
-            case 'CD':
-            case 'CM':
-                output_number += int(RomanToInteger[s[i+1]]) - int(RomanToInteger[s[i]]);
-                break;
-            default:
-                output_number += int(RomanToInteger[s[i+1]]);
-                break;
-        }       
+    cout << s[2] << endl;
+    for (int i = 0; i < s.length() - 1; ++i){
+        RomanToInteger[s[i+1]] < RomanToInteger[s[i]] ? output_number += RomanToInteger[s[i]] : output_number -= RomanToInteger[s[i]];
     }
+    output_number += RomanToInteger[s[s.length() -1]];
     return output_number;
+}
+
+int main(int args, char **argv){
+
+    string roman;
+    cout << "Please input roman number:";
+    cin >> roman;
+    cout << romanToInt(roman) << endl;
+    return 0;
 }
