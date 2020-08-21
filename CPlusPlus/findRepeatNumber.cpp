1307630e273d6e1adacd57decec31bea84c077d8
zmingfeng <xycg.sky@gmail.com>
2020-08-21 21:53:17 +0800

    add comments

--- CPlusPlus/findRepeatNumber.cpp
+++ CPlusPlus/findRepeatNumber.cpp
@@ -6,15 +6,19 @@ using namespace std;
 
 int findRepeatNumber(vector<int> &nums)
 {
+    // 定义一个跟原数组一样大小的数组
     vector<int> temp_index(nums.size(), 0);
+    // 遍历数组
     for (auto i = 0; i < nums.size(); ++i)
     {
+        // 只要元素个数不为0，再次遇到即可返回
         if (temp_index[nums[i]] > 0)
         {
             return nums[i];
         }
         else
         {
+            // 对原数组中的数字进行计数
             temp_index[nums[i]] += 1;
         }
     }
